ecd75168707e1fc82999c2ec257fd4af820c0739
oussamahamani <oussamahamani2000@gmail.com>
2022-02-01 18:37:39 -0500

    initial commit-starting the structure

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Nav.css"
+const Nav = () => {
+    return (
+        <div id="nav-box">
+<h1 id="nav-home">HI,Sarah</h1>        
+<h1> Sarah</h1>        
+<h1>about Sarah</h1>        
+
+</div>
+    );
+}
+
+export default Nav;
