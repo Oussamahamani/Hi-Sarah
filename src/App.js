7400a9a82f333bd53e2a8d3545af5431db6606ea
oussamahamani <oussamahamani2000@gmail.com>
2022-02-02 10:59:25 -0500

    showing messages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+import Sarah from "./pages/Sarah";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      
-      <Footer />
+       <Nav />
+      < Sarah/>
+      <Footer /> 
     </div>
   );
 }
--- src/pages/Sarah.js
+++ src/pages/Sarah.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import { useState } from 'react'; 
+import axios from 'axios'
+
 
 const Sarah = () => {
-    return (
 
-        
+const [Message, setMessage] = useState('');
+const [Sara, setSara] = useState('');
+const [Messages, setMessages] = useState([]);
+
+
+const Addmessages =(message) =>{
+    setMessages((preMessages)=>{
+        return [...preMessages, {name:message }]
+    })
+}
+const fetchSara = async () => {
+    try {
+        const response = await axios.get(`http://api.brainshop.ai/get?bid=163044&key=tL53y0qRo8Ztpqfv&uid=[1]&msg=${Message}`)
+                console.log(response)
+                setSara(response.data.cnt)
+                Addmessages(Sara)      
+            } catch (error) {
+                console.error(error)
+                console.log('not working')
+    } 
+}
+
+const handleSubmit =(e) =>{
+e.preventDefault()
+Addmessages(Message)
+fetchSara()
+setMessage('')
+
+}
+    return (
         <div>
-            
+            {Messages.map(message=>(
+                <div>
+                <h3>{message.name}</h3>
+                </div>
+            ))}
+ 
+          <form onSubmit={handleSubmit}>
+                <label >            
+                <input type='text' onChange={(e)=> setMessage(e.target.value)} value={Message}></input>
+                <button>send message</button>
+                </label>
+            </form>
         </div>
     );
 }
