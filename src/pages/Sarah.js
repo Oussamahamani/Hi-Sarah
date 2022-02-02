f77631a159d35633ec9b7f87be0d26ef90a20998
oussamahamani <oussamahamani2000@gmail.com>
2022-02-02 13:58:15 -0500

    success

--- src/pages/Sarah.js
+++ src/pages/Sarah.js
@@ -16,14 +16,31 @@ const Addmessages =(message) =>{
     })
 }
 const fetchSara = async () => {
-    try {
-        const response = await axios.get(`http://api.brainshop.ai/get?bid=163410&key=hWlvyscvNLHzjlmQ&uid=[1]&msg=${Message}`)
-                console.log(response.data.cnt)
-                setSara(response.data.cnt)
-            } catch (error) {
-                console.error(error)
-                console.log('not working')
-    } 
+    const options = {
+        method: 'GET',
+        url: 'https://http-cors-proxy.p.rapidapi.com/http://api.brainshop.ai/get',
+        params: {bid: '163410', key: 'hWlvyscvNLHzjlmQ', uid: '[1]', msg: Message},
+        headers: {
+          origin: 'example.com',
+          'x-requested-with': 'example.com',
+          'x-rapidapi-host': 'http-cors-proxy.p.rapidapi.com',
+          'x-rapidapi-key': '7e16663340mshbfb2c833d8dcaffp1f960fjsnc96a63bc6c4c'
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+          setSara(response.data.cnt)
+      }).catch(function (error) {
+          console.error(error);
+      });
+    // try {
+    //     const response = await axios.get(`https://thingproxy.freeboard.io/http://api.brainshop.ai/get?bid=163410&key=hWlvyscvNLHzjlmQ&uid=[1]&msg=${Message}`)
+    //             console.log(response.data.cnt)
+    //         } catch (error) {
+    //             console.error(error)
+    //             console.log('not working')
+    // } 
 }
 
 useEffect(() => {
