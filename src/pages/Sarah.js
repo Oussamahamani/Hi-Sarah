c0fb85a0f35c2e828730066b46b4487d2cfb05dc
oussamahamani <oussamahamani2000@gmail.com>
2022-02-02 17:35:00 -0500

    you can talk with sarah, and sarah can understand other languages

--- src/pages/Sarah.js
+++ src/pages/Sarah.js
@@ -8,6 +8,9 @@ const Sarah = () => {
 const [Message, setMessage] = useState('');
 const [Sara, setSara] = useState('');
 const [Messages, setMessages] = useState([]);
+const [Language, setLanguage] = useState();
+const [Translation, setTranslation] = useState();
+const [Final, setFinal] = useState();
 
 
 const Addmessages =(message) =>{
@@ -15,11 +18,50 @@ const Addmessages =(message) =>{
         return [...preMessages, {name:message }]
     })
 }
+
+const Detectlanguage = async (Translation, Language) => {
+const options = {
+    method: 'POST',
+    url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
+    params: {'api-version': '3.0', to: 'en', textType: 'plain', profanityAction: 'NoAction'},
+    headers: {
+      'content-type': 'application/json',
+      'x-rapidapi-host': 'microsoft-translator-text.p.rapidapi.com',
+      'x-rapidapi-key': '7e16663340mshbfb2c833d8dcaffp1f960fjsnc96a63bc6c4c'
+    },
+    data: [{Text: Message}]
+  };
+  
+  axios.request(options).then(function (response) {
+      setTranslation(response.data[0].translations[0].text);
+      setLanguage( response.data[0].detectedLanguage.language)
+    //   console.log(Translation)
+  }).catch(function (error) {
+      console.error(error);
+  });
+}
+
+const clean = () =>{
+    setMessage('')
+    setSara('')
+    setTranslation('')
+    setLanguage('')
+}
+
+// const fetching = async () => {
+// if (Language === 'en'){
+//     /// fetch sara
+// }
+// else{ Detectlanguage(Message, Language)
+// }
+// }
+
+
 const fetchSara = async () => {
     const options = {
         method: 'GET',
         url: 'https://http-cors-proxy.p.rapidapi.com/http://api.brainshop.ai/get',
-        params: {bid: '163410', key: 'hWlvyscvNLHzjlmQ', uid: '[1]', msg: Message},
+        params: {bid: '163410', key: 'hWlvyscvNLHzjlmQ', uid: '[1]', msg: Translation},
         headers: {
           origin: 'example.com',
           'x-requested-with': 'example.com',
@@ -30,7 +72,8 @@ const fetchSara = async () => {
       
       axios.request(options).then(function (response) {
           console.log(response.data);
-          setSara(response.data.cnt)
+          setSara(sa => response.data.cnt)
+            setMessage(response.data.cnt)
       }).catch(function (error) {
           console.error(error);
       });
@@ -44,14 +87,23 @@ const fetchSara = async () => {
 }
 
 useEffect(() => {
-    Addmessages(Sara)
-console.log('last push')
+if (Translation){fetchSara()
+console.log('fetch')}
+}, [Translation]);
+
+
+useEffect(() => {
+ if (Sara){ Addmessages(Sara)
+  console.log('sara')}
+  clean()
 }, [Sara]);
+
 const handleSubmit =(e) =>{
+
 e.preventDefault()
 Addmessages(Message)
-fetchSara()
-setMessage('')
+Detectlanguage(Message)
+
 
 }
     return (
